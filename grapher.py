be06545876f3354c338d10ab3a03b1732173f1ef
jakeshirey <94328784+jakeshirey@users.noreply.github.com>
2022-06-10 14:47:42 -0400

    Add files via upload
    horseGUI.py now includes splitter to adjust size of graph vs video. grapher.py includes file dialog

--- grapher.py
+++ grapher.py
@@ -23,37 +23,27 @@ class DataDisplay(qtw.QWidget):
         super().__init__()
 
         #self.setWindowTitle("Horse Data Visualization Tool")
-        graphLayout = qtw.QVBoxLayout()
-
-
-        self.setLayout(graphLayout)
-
+        
+        #create a label for the graph
         self.graph_label = qtw.QLabel("Ankle Position by Time")
         self.graph_label.setFont(qtg.QFont(''))
         self.graph_label.setFixedHeight(50)
-        graphLayout.addWidget(self.graph_label)
+
+        #create open-file button
+        self.openBtn = qtw.QPushButton('Open CSV Data')
+        self.openBtn.clicked.connect(self.open_file)
         
         # Create the maptlotlib FigureCanvas object,
         # which defines a single set of axes as self.axes.
         self.graph = MplCanvas(self, width=5, height=4, dpi=100)
-
-        frame = []
-        yPos = []
-        with open('C0213DLC_resnet50_NewSkeletonMay27shuffle1_1030000.csv', 'r') as csvfile:
-            lines = csv.reader(csvfile, delimiter=',')
-            for row in lines:
-                try:
-                    frame.append(float(row[0])) #append the numeric values
-                    yPos.append(float(row[2]))
-                except:
-                    pass #ignore the nonnumeric values
-
-        self.graph.axes.plot(frame, yPos, label = "Right Front Hoof")
-        self.graph.axes.legend()
-        #self.graph.axes._label('Frame')
-        #self.graph.ylabel("Y Position")
         self.graph.setMinimumSize(480, 270)
+
+        #add widgets to layout
+        graphLayout = qtw.QVBoxLayout()
+        graphLayout.addWidget(self.graph_label)
         graphLayout.addWidget(self.graph)
+        graphLayout.addWidget(self.openBtn)
+        self.setLayout(graphLayout)
 
         self.update()
         self.show()
@@ -61,5 +51,25 @@ class DataDisplay(qtw.QWidget):
     #Button Functionality
     def pressed(self):
         pass
+
+    def open_file(self):
+        filename, _ = qtw.QFileDialog.getOpenFileName(self, "Open CSV Data")
+
+        if filename != '':
+            frame = []
+            yPos = []
+            with open(filename, 'r') as csvfile:
+                lines = csv.reader(csvfile, delimiter=',')
+                for row in lines:
+                    try:
+                        frame.append(float(row[0])) #append the numeric values
+                        yPos.append(float(row[2]))
+                    except:
+                        pass #ignore the nonnumeric values
+            
+            self.graph.axes.cla()
+            self.graph.axes.plot(frame, yPos, label = "Right Front Hoof")
+            self.graph.axes.legend()
+            self.graph.draw_idle()
 
 #Initialize
--- horseGUI.py
+++ horseGUI.py
@@ -1,11 +1,6 @@
 import sys
 from xml.etree.ElementTree import tostring
 
-import matplotlib
-matplotlib.use('Qt5Agg')
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
-
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
 
@@ -17,50 +12,24 @@ class MainWindow(qtw.QWidget):
         super().__init__()
 
         self.setWindowTitle("Horse Data Visualization Tool")
-        outerLayout = qtw.QHBoxLayout()
-        videoLayout = qtw.QVBoxLayout()
-        graphLayout = qtw.QVBoxLayout()
-
-        outerLayout.addLayout(videoLayout)
-        outerLayout.addLayout(graphLayout)
-        self.setLayout(outerLayout)
-
         
-        self.over_label = qtw.QLabel("Horsey!!")
-        self.over_label.setFont(qtg.QFont(''))
-        self.over_label.setFixedHeight(50)
-        videoLayout.addWidget(self.over_label)
-        
-
-        self.toggle_skeleton = qtw.QPushButton("Toggle Skeleton Overlay")
-        self.toggle_skeleton.setCheckable(True)
-        self.toggle_skeleton.toggle()
-        self.toggle_skeleton.clicked.connect(self.pressed)
-        videoLayout.addWidget(self.toggle_skeleton)
-
         self.videoplayer = vid.VideoPlayer()
         self.videoplayer.setMinimumSize(480,270)
-        videoLayout.addWidget(self.videoplayer)
-        '''
-        self.horse_pic = qtw.QLabel()
-        self.horsemap = qtg.QPixmap('horse.jpg')
-        self.skeletonmap = qtg.QPixmap('horse-skeleton.jpg')
-        self.horse_pic.setPixmap(self.horsemap)
-        videoLayout.addWidget(self.horse_pic)
-        '''
         
-        # Create the maptlotlib FigureCanvas object,
-        # which defines a single set of axes as self.axes.
         self.graph = grapher.DataDisplay()
         self.graph.setMinimumSize(480, 270)
-        graphLayout.addWidget(self.graph)
+
+        splitter = qtw.QSplitter()
+        splitter.addWidget(self.videoplayer)
+        splitter.addWidget(self.graph)
+
+        outerLayout = qtw.QVBoxLayout()
+        outerLayout.addWidget(splitter)
+        self.setLayout(outerLayout)
 
         self.update()
         self.show()
 
-    #Button Functionality
-    def pressed(self):
-        pass
 
 #Initialize
 app = qtw.QApplication([])
